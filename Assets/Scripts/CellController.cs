044fdddf73604364f03f5bbd43f0fb13a72b3890
kim jea hyun <wogus3204@gmail.com>
2024-02-29 14:09:29 +0900

    209

--- Assets/Scripts/CellController.cs
+++ Assets/Scripts/CellController.cs
@@ -4,6 +4,7 @@ using TMPro;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 
 [System.Serializable]
@@ -48,7 +49,8 @@ public class CellController : MonoBehaviour
     [SerializeField] TextMeshProUGUI speciesText;
     [SerializeField] TextMeshProUGUI companyText;
     [SerializeField] GameObject detailPanel;
-   
+
+
 
     // Public properties to access values
     public float InTpValue { get; private set; }
@@ -60,9 +62,12 @@ public class CellController : MonoBehaviour
     public float InCo2Value { get; private set; }  // New variable
     private string apiUrl;
     public string formid;
-    public Transform detaillocal;
+    Canvas canvas;
 
-   
+
+
+
+
 
     void Awake()
     {
@@ -99,17 +104,19 @@ public class CellController : MonoBehaviour
 
     public void Start()
     {
-       
+        // 스크립트에서 Scroll View 찾아서 할당
+        canvas = GameObject.Find("Canvas").GetComponent<Canvas>();
     }
 
     public void Update()
     {
+
     }
 
     IEnumerator GetApiData()
     {
-        formid = noText.text;
-        apiUrl = $"http://apis.data.go.kr/1390000/SmartFarmdata/envdatarqst?serviceKey=ndExmAZPa6Z1SBWydoZsH8RFcdL6XjiFlmZ4Qe0LVdu6WyGJJpkvYMB5ecMII4AIXi0P%2BYcuqLKslBw6ILFgbA%3D%3D&searchFrmhsCode={formid}&returnType=json"; 
+        
+        apiUrl = $"http://apis.data.go.kr/1390000/SmartFarmdata/envdatarqst?serviceKey=ndExmAZPa6Z1SBWydoZsH8RFcdL6XjiFlmZ4Qe0LVdu6WyGJJpkvYMB5ecMII4AIXi0P%2BYcuqLKslBw6ILFgbA%3D%3D&searchFrmhsCode={formid}&returnType=json";
         using (UnityWebRequest webRequest = UnityWebRequest.Get(apiUrl))
         {
             yield return webRequest.SendWebRequest();
@@ -129,21 +136,58 @@ public class CellController : MonoBehaviour
                     InHdValue = measurementItem.inHd;
                     FrmhsIdValue = measurementItem.frmhsId;
                     OutWsValue = measurementItem.outWs;
-                    MeasDtSrValue = measurementItem.measDtStr; // 수정: 변수명을 맞춤
+                    MeasDtSrValue = measurementItem.measDtStr;
                     OutTpValue = measurementItem.outTp;
                     InCo2Value = measurementItem.inCo2;
-
                 }
             }
-            Debug.Log($"inTp: {InTpValue}, inHd: {InHdValue}, frmhsId: {FrmhsIdValue}, measDtStr: {MeasDtSrValue}, outWs: {OutWsValue}, outTp: {OutTpValue}, inCo2: {InCo2Value}");
-            Instantiate(detailPanel,detaillocal);
-        }
 
-      
+            Debug.Log($"inTp: {InTpValue}, inHd: {InHdValue}, frmhsId: {FrmhsIdValue}, measDtStr: {MeasDtSrValue}, outWs: {OutWsValue}, outTp: {OutTpValue}, inCo2: {InCo2Value}");
+
+            // Instantiate the detailPanel prefab
+            GameObject instantiatedPanel = Instantiate(detailPanel, canvas.transform);  // Assuming scrollView is the Scroll View in your scene
+
+            // Access TextMeshProUGUI components in the instantiatedPanel and update their values
+            TextMeshProUGUI[] textComponents = instantiatedPanel.GetComponentsInChildren<TextMeshProUGUI>();
+
+            // Update text values based on the fetched API data
+            foreach (TextMeshProUGUI textComponent in textComponents)
+            {
+                switch (textComponent.name)
+                {
+                    case "InTpText":
+                        textComponent.text = $"InTp: {InTpValue}";
+                        break;
+                    case "InHdText":
+                        textComponent.text = $"InHd: {InHdValue}";
+                        break;
+                    case "FrmhsIdText":
+                        textComponent.text = $"FrmhsId: {FrmhsIdValue}";
+                        break;
+                    case "OutWsText":
+                        textComponent.text = $"OutWs: {OutWsValue}";
+                        break;
+                    case "MeasDtStrText":
+                        textComponent.text = $"MeasDtStr: {MeasDtSrValue}";
+                        break;
+                    case "OutTpText":
+                        textComponent.text = $"OutTp: {OutTpValue}";
+                        break;
+                    case "InCo2Text":
+                        textComponent.text = $"InCo2: {InCo2Value}";
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     }
     public void OnClickButton()
     {
+        formid = noText.text;
         // Fetch API data when the button is clicked
         StartCoroutine(GetApiData());
+        // Instantiate the detailPanel prefab
+
     }
 }
--- Assets/Scripts/DictionarySample.cs
+++ Assets/Scripts/DictionarySample.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class DictionarySample : MonoBehaviour
 {
-    /*
     // 셀이 만들어질 Trasform 값과 가져올 프리팹 GameObject를 SerializeField로 추가
     [SerializeField] Transform content;
     [SerializeField] GameObject cellPrefab;
@@ -22,53 +21,25 @@ public class DictionarySample : MonoBehaviour
         corp2["species"] = "";
         corp2["company"] = "";
 
-        Dictionary<string, string> corp3 = new Dictionary<string, string>();
-        corp3["no"] = "";
-        corp3["species"] = "";
-        corp3["company"] = "";
-
-        Dictionary<string, string> corp4 = new Dictionary<string, string>();
-        corp4["no"] = "";
-        corp4["species"] = "";
-        corp4["company"] = "";
-
-        Dictionary<string, string> corp5 = new Dictionary<string, string>();
-        corp5["no"] = "";
-        corp5["species"] = "";
-        corp5["company"] = "";
 
         List<Dictionary<string, string>> corpBook = new List<Dictionary<string, string>>();
         corpBook.Add(corp1);
-        corpBook.Add(corp2);
-        corpBook.Add(corp3);
-        corpBook.Add(corp4);
-        corpBook.Add(corp5);
+        corpBook.Add(corp2);      
 
         // 불러오기
         Dictionary<string, string> dict = corpBook[0];
         Debug.Log(dict["company"]);
 
-        //프리팹 Cell을 가져와서 ScrollView의 Content 안에 생성
-        GameObject cellObject1 = Instantiate(cellPrefab, content);
-        CellController cellController1 = cellObject1.GetComponent<CellController>(); //CellController를 불러오기
-        cellController1.SetData(corp1); //corp1 전달
+        ////프리팹 Cell을 가져와서 ScrollView의 Content 안에 생성
+        //GameObject cellObject1 = Instantiate(cellPrefab, content);
+        //CellController cellController1 = cellObject1.GetComponent<CellController>(); //CellController를 불러오기
+        //cellController1.SetData(corp1); //corp1 전달
+        //
+        //GameObject cellObject2 = Instantiate(cellPrefab, content);
+        //CellController cellController2 = cellObject2.GetComponent<CellController>(); //CellController를 불러오기
+        //cellController2.SetData(corp2); //corp2 전달
 
-        GameObject cellObject2 = Instantiate(cellPrefab, content);
-        CellController cellController2 = cellObject2.GetComponent<CellController>(); //CellController를 불러오기
-        cellController2.SetData(corp2); //corp2 전달
-
-        GameObject cellObject3 = Instantiate(cellPrefab, content);
-        CellController cellController3 = cellObject3.GetComponent<CellController>(); //CellController를 불러오기
-        cellController3.SetData(corp3); //corp3 전달
-
-        GameObject cellObject4 = Instantiate(cellPrefab, content);
-        CellController cellController4 = cellObject4.GetComponent<CellController>(); //CellController를 불러오기
-        cellController4.SetData(corp4); //corp4 전달
-
-        GameObject cellObject5 = Instantiate(cellPrefab, content);
-        CellController cellController5 = cellObject5.GetComponent<CellController>(); //CellController를 불러오기
-        cellController5.SetData(corp5); //corp5 전달
 
     }
-    */
+
 }
